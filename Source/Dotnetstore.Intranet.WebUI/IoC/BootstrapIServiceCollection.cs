17ec5a4377a8b525be44f48b07f61dc44f1c64a2
Hans Sjödin <hasse29@hotmail.com>
2023-02-02 11:11:07 +0100

    Added mudblazor

---
+++ Source/Dotnetstore.Intranet.WebUI/IoC/BootstrapIServiceCollection.cs
@@ -0,0 +1,11 @@
+﻿using MudBlazor.Services;
+
+namespace Dotnetstore.Intranet.WebUI.IoC;
+
+public static class BootstrapIServiceCollection
+{
+    internal static void Build(ref IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddMudServices();
+    }
+}
